8855374f1ad858e4aa86c62351259ff2c3093105
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-09-27 17:16:18 -0500

    wins and losses counters added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,14 @@ $(document).ready(function () {
     var losses = 0;
     var myNumber = 0;
 
+    $('#wins-counter').text(wins);
+    $('#losses-counter').text(losses);
+    $("#my-number").text(myNumber);
 
     //wrapping a startGame option aroud functions -- call start game
 
     function startGame() {
+
         //create randomly generated number
         var targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
         $("#target-number").text(targetNumber);
@@ -19,32 +23,32 @@ $(document).ready(function () {
         $("#gem1-btn").on("click", function () {
             console.log(gem1);
             //add crystal value to myNumber
-            gem1Value = parseInt(gem1);
-            gem1Value += myNumber;
+            var gem1Value = parseInt(gem1);
+            myNumber += gem1Value;
         })
 
-        var gem2 = Math.floor(Math.random() * 11 + 1); console.log(gem2);
+        var gem2 = Math.floor(Math.random() * 11 + 1);
         $("#gem2-btn").on("click", function () {
             console.log(gem2);
             //add crystal value to myNumber
-            gem1Value = parseInt(gem2);
-            gem1Value += myNumber;
+            var gem2Value = parseInt(gem2);
+            myNumber += gem2Value;
         })
 
-        var gem3 = Math.floor(Math.random() * 11 + 1); console.log(gem3);
+        var gem3 = Math.floor(Math.random() * 11 + 1);
         $("#gem3-btn").on("click", function () {
             console.log(gem3);
             //add crystal value to myNumber
-            gem1Value = parseInt(gem3);
-            gem1Value += myNumber;
+            var gem3Value = parseInt(gem3);
+            myNumber += gem3Value;
         })
 
-        var gem4 = Math.floor(Math.random() * 11 + 1); console.log(gem4);
+        var gem4 = Math.floor(Math.random() * 11 + 1);
         $("#gem4-btn").on("click", function () {
             console.log(gem4);
             //add crystal value to myNumber
-            gem1Value = parseInt(gem4);
-            gem1Value += myNumber;
+            var gem4Value = parseInt(gem4);
+            myNumber += gem4Value;
         })
 
         $("#myNumber").text(myNumber);

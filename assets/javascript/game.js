5829466829d417d4c95c32e600a8aa81e5fce214
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-09-27 22:45:13 -0500

    comparison code working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,94 +3,109 @@ $(document).ready(function () {
     //variables to hold game values
     var wins = 0;
     var losses = 0;
-    var myNumber = 0;
-    var targetNumber = 0;
+    var targetNumber;
+    var myNumber;
+    var newNumber;
 
-    $('#wins-counter').text(wins);
-    $('#losses-counter').text(losses);
 
 
     //wrapping a startGame option aroud functions -- call start game
 
     function startGame() {
+        var targetNumber = 0;
+        var myNumber = 0;
+        var wins = 0;
+        var losses = 0;
+        $('#wins-counter').text(wins);
+        $('#losses-counter').text(losses);
+
 
         //create randomly generated number
-        targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+        var targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
         $("#target-number").text(targetNumber);
         console.log(targetNumber);
 
         // assign random number to crystals
         var gem1 = Math.floor(Math.random() * 11 + 1);
         $("#gem1-btn").on("click", function () {
-            console.log(gem1);
+            console.log("Gem 1 Value: " + gem1);
             myNumber = myNumber + gem1;
-            console.log("newNumber = " + myNumber);
+            // console.log("newNumber = " + myNumber);
             $("#my-number").text(myNumber);
+
             if (myNumber === targetNumber) {
+                alert("You win!");
                 wins++;
-                //if the number is greater than the target number losses
             }
-            else if (myNumber >= targetNumber) {
+            else if (myNumber > targetNumber) {
+                alert("Looks like you're going to have to try again")
                 losses++;
             }
 
         })
+
+
+        var gem2 = Math.floor(Math.random() * 11 + 1);
+        $("#gem2-btn").on("click", function () {
+            console.log("Gem 2 Value: " + gem2);
+            myNumber = myNumber + gem2;
+            console.log("newNumber = " + myNumber);
+            $("#my-number").text(myNumber);
+            if (myNumber === targetNumber) {
+                alert("You win!");
+                wins++;
+            }
+            else if (myNumber > targetNumber) {
+                alert("Looks like you're going to have to try again")
+                losses++;
+            }
+
+        })
+
+
+        var gem3 = Math.floor(Math.random() * 11 + 1);
+        $("#gem3-btn").on("click", function () {
+            console.log(gem3);
+            myNumber = myNumber + gem3;
+            console.log("newNumber = " + myNumber);
+            $("#my-number").text(myNumber);
+            if (myNumber === targetNumber) {
+                alert("You win!");
+                wins++;
+            }
+            else if (myNumber > targetNumber) {
+                alert("Looks like you're going to have to try again")
+                losses++;
+            }
+
+        })
+
+
+        var gem4 = Math.floor(Math.random() * 11 + 1);
+        $("#gem4-btn").on("click", function () {
+            console.log(gem4);
+            myNumber = myNumber + gem4;
+            console.log("newNumber = " + myNumber);
+            $("#my-number").text(myNumber);
+            if (myNumber === targetNumber) {
+                alert("You win!");
+                wins++;
+                startGame();
+            }
+            else if (myNumber >= targetNumber) {
+                alert("Looks like you're going to have to try again!")
+                losses++;
+                startGame();
+            }
+        })
     }
-
-    var gem2 = Math.floor(Math.random() * 11 + 1);
-    $("#gem2-btn").on("click", function () {
-        console.log(gem2);
-        myNumber = myNumber + gem2;
-        console.log("newNumber = " + myNumber);
-        $("#my-number").text(myNumber);
-        if (myNumber === targetNumber) {
-            wins++;
-            //if the number is greater than the target number losses
-        }
-        else if (myNumber >= targetNumber) {
-            losses++;
-        }
-
-    })
-
-
-    var gem3 = Math.floor(Math.random() * 11 + 1);
-    $("#gem3-btn").on("click", function () {
-        console.log(gem3);
-        myNumber = myNumber + gem3;
-        console.log("newNumber = " + myNumber);
-        $("#my-number").text(myNumber);
-        if (myNumber === targetNumber) {
-            wins++;
-            //if the number is greater than the target number losses
-        }
-        else if (myNumber >= targetNumber) {
-            losses++;
-        }
-
-    })
-
-
-    var gem4 = Math.floor(Math.random() * 11 + 1);
-    $("#gem4-btn").on("click", function () {
-        console.log(gem4);
-        myNumber = myNumber + gem4;
-        console.log("newNumber = " + myNumber);
-        $("#my-number").text(myNumber);
-        if (myNumber === targetNumber) {
-            wins++;
-            //if the number is greater than the target number losses
-        }
-        else if (myNumber >= targetNumber) {
-            losses++;
-        }
-    })
     startGame();
+    //reset function
+    $("#start-btn").on("click", function () {
+        startGame();
+    })
 });
 
-//Reset Game
-
-// function resetGame {
 
 // }
 
